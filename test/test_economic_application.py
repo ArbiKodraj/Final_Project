10187c30ea13eedbf17938d1cd77b24d34c56f5d
ArbiKodraj <64042738+ArbiKodraj@users.noreply.github.com>
2021-02-23 17:16:21 +0100

    additional part

--- test/test_economic_application.py
+++ test/test_economic_application.py
@@ -8,88 +8,80 @@ from economic_application import demand, supply
 from economic_application import PolynomialDS
 from economic_application import AISupplyDemandApprox
 
+
 class Test_demand_supply(unittest.TestCase):
-    """This object aims to test the demand and supply function. 
+    """This object aims to test the demand and supply function.
     It is created by subclassing :class:`unittest.TestCase.`.
-    
+
     Args:
         unittest (object):  Base class, which may be used to create new test cases.
     """
+
     def test_demand(self):
-        """Tests demand function.
-        """
-        self.assertRaises(TypeError, demand, [1,2,3])
-        self.assertRaises(TypeError, demand, [-1,-3])
+        """Tests demand function."""
+        self.assertRaises(TypeError, demand, [1, 2, 3])
+        self.assertRaises(TypeError, demand, [-1, -3])
         self.assertRaises(TypeError, demand, 2)
         self.assertEqual(
-            np.array([np.nan, np.nan, np.nan]).all(), 
-            demand(np.array([0.1, 1, 0.2])).all()
-            )
+            np.array([np.nan, np.nan, np.nan]).all(),
+            demand(np.array([0.1, 1, 0.2])).all(),
+        )
 
     def test_supply(self):
-        """Tests supply function.
-        """
-        self.assertRaises(TypeError, supply, [1,2,3])
-        self.assertRaises(TypeError, supply, [-1,-3])
+        """Tests supply function."""
+        self.assertRaises(TypeError, supply, [1, 2, 3])
+        self.assertRaises(TypeError, supply, [-1, -3])
         self.assertRaises(TypeError, supply, 2)
         self.assertEqual(
             np.array([1.0, 1.0, 1.5, 3.0, 4.2, 12.0, 20.0]).all(),
-            supply(np.array([8, 3, 12, 20, 44, 79, 99])).all() 
-            )
+            supply(np.array([8, 3, 12, 20, 44, 79, 99])).all(),
+        )
+
 
 class Test_polynomial(unittest.TestCase):
-    """This object aims to test the :class:`PolynomialDS` object. 
+    """This object aims to test the :class:`PolynomialDS` object.
 
     Args:
         unittest (object):  Base class, which may be used to create new test cases.
     """
+
     def test_construction(self):
-        """Tests constructor.
-        """
-        self.assertRaises(
-            AssertionError, PolynomialDS, -1, 10, 10, demand, supply
-            )
-        self.assertRaises(
-            AssertionError, PolynomialDS, 10, 200, 10, demand, supply
-            )
+        """Tests constructor."""
+        self.assertRaises(AssertionError, PolynomialDS, -1, 10, 10, demand, supply)
+        self.assertRaises(AssertionError, PolynomialDS, 10, 200, 10, demand, supply)
 
     def test_length(self):
-        """Tests length method.
-        """
+        """Tests length method."""
         nodes = 100
-        self.assertEqual(
-            nodes, len(PolynomialDS(1, 50, nodes, demand, supply))
-            )
-    
+        self.assertEqual(nodes, len(PolynomialDS(1, 50, nodes, demand, supply)))
+
+
 class Test_AI(unittest.TestCase):
     """This object aims to test the :class:`AISupplyDemandApprox` object.
 
     Args:
         unittest (object):  Base class, which may be used to create new test cases.
     """
+
     def test_construction(self):
-        """Tests object construction.
-        """
+        """Tests object construction."""
         self.assertRaises(
             AssertionError, AISupplyDemandApprox, 100, supply, demand, a=-1
-            )
+        )
         obj = AISupplyDemandApprox(100, supply, demand)
         self.assertNotIn(np.nan, obj.pd_train)
         self.assertNotIn(np.nan, obj.pd_test)
 
     def test_name(self):
-        """Tests object's name.
-        """
+        """Tests object's name."""
         obj = AISupplyDemandApprox(200, supply, demand)
         self.assertIsInstance(obj.__name__(), str)
 
     def test_plot(self):
-        """Tests plot method.
-        """
+        """Tests plot method."""
         obj = AISupplyDemandApprox(200, supply, demand)
-        self.assertRaises(
-            AssertionError, obj.plots, degrees=[3,6,10]
-            )
+        self.assertRaises(AssertionError, obj.plots, degrees=[3, 6, 10])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
--- test/test_stockprice_prediction.py
+++ test/test_stockprice_prediction.py
@@ -9,152 +9,113 @@ from stock_price_predictor import _assign_data, _assignment_for_future_predictio
 from stock_price_predictor import StockPricePredictor
 from stock_price_predictor import NetworkPricePredictor
 
+
 class Test_StockPricePredictor(unittest.TestCase):
-    """Object that aims to test some specifications of the 
-    :class:`StockPricePredictor` class. It assures that methods 
-    do what they are supposed to do. It is created by subclassing 
+    """Object that aims to test some specifications of the
+    :class:`StockPricePredictor` class. It assures that methods
+    do what they are supposed to do. It is created by subclassing
     :class:`unittest.TestCase`.
-    
+
     Args:
         unittest (object): Base class, which may be used to create new test cases.
     """
+
     def test_construction(self):
-        """Tests constructor method.
-        """
-        self.assertIsInstance(
-            StockPricePredictor('aapl').ticker, str
-            )
-        
+        """Tests constructor method."""
+        self.assertIsInstance(StockPricePredictor("aapl").ticker, str)
+
     def test_prep(self):
-        """Tests training and testing data preperation method.
-        """
-        aapl = StockPricePredictor('aapl')
-        aapl.__repr__() 
+        """Tests training and testing data preperation method."""
+        aapl = StockPricePredictor("aapl")
+        aapl.__repr__()
         for _ in range(4):
             if _ < 2:
-                self.assertEqual(
-                    type(aapl._data_prep()[_]).__name__, 'DatetimeIndex'
-                ) 
+                self.assertEqual(type(aapl._data_prep()[_]).__name__, "DatetimeIndex")
             else:
-                self.assertEqual(
-                    type(aapl._data_prep()[_]).__name__, 'Series'
-                )      
-    
+                self.assertEqual(type(aapl._data_prep()[_]).__name__, "Series")
+
     def test_data_and_mlp(self):
-        """Tests data preperation and mlp preperation method.
-        """
-        aapl = StockPricePredictor('aapl')
+        """Tests data preperation and mlp preperation method."""
+        aapl = StockPricePredictor("aapl")
         aapl.__repr__()
         for i in range(4):
             if i != 2:
-                self.assertIs(
-                    type(aapl._predict_price_knn()[i]),
-                    np.ndarray
-                )
+                self.assertIs(type(aapl._predict_price_knn()[i]), np.ndarray)
             else:
-                self.assertIs(
-                    type(aapl._predict_price_knn()[i]),
-                    pd.DatetimeIndex
-                )
-        self.assertRaises(
-            AssertionError, aapl._nn_model, h1_layer=12.3, h2_layer=10.1
-        )
-        self.assertRaises(
-            AssertionError, aapl._nn_model, iterations=10.1
-        )
-        self.assertRaises(
-            ValueError, aapl._nn_model, learning_rate=1
-        )
-        self.assertRaises(
-            ValueError, aapl._nn_model, learning_rate=-1
-        )
-        self.assertRaises(
-            ValueError, aapl._nn_model, h1_layer=-10
-        )
-        self.assertRaises(
-            ValueError, aapl._nn_model, iterations=-1000
-        )
+                self.assertIs(type(aapl._predict_price_knn()[i]), pd.DatetimeIndex)
+        self.assertRaises(AssertionError, aapl._nn_model, h1_layer=12.3, h2_layer=10.1)
+        self.assertRaises(AssertionError, aapl._nn_model, iterations=10.1)
+        self.assertRaises(ValueError, aapl._nn_model, learning_rate=1)
+        self.assertRaises(ValueError, aapl._nn_model, learning_rate=-1)
+        self.assertRaises(ValueError, aapl._nn_model, h1_layer=-10)
+        self.assertRaises(ValueError, aapl._nn_model, iterations=-1000)
+
 
 class Test_NetworkPricePredictor(unittest.TestCase):
-    """Object that aims to test some specifications of the 
-    :class:`NetworkPricePredictor` class. The :class:`NetworkPricePredictor` 
+    """Object that aims to test some specifications of the
+    :class:`NetworkPricePredictor` class. The :class:`NetworkPricePredictor`
     class is based on the :class:`StockPricePredictor` class.
-    It also assures that methods do what they are supposed to do. 
+    It also assures that methods do what they are supposed to do.
     It is created by subclassing :class:`unittest.TestCase`.
-    
+
     Args:
         unittest (object): Base class, which may be used to create new test cases.
     """
+
     def test_split_data(self):
-        """Tests split data method.
-        """
-        nio = NetworkPricePredictor('nio')
+        """Tests split data method."""
+        nio = NetworkPricePredictor("nio")
         for j in range(2):
-            self.assertIsInstance(
-                nio._split_data()[j],
-                np.ndarray
-            )
+            self.assertIsInstance(nio._split_data()[j], np.ndarray)
 
     def test_assign_data(self):
-        """Tests assign data method.
-        """
-        nio = NetworkPricePredictor('nio')
-        self.assertRaises(
-            TypeError,
-            nio.assign_data,
-            plot=10
-        )
+        """Tests assign data method."""
+        nio = NetworkPricePredictor("nio")
+        self.assertRaises(TypeError, nio.assign_data, plot=10)
 
     def test__assign_data(self):
-        """Tests _assign data method.
-        """
-        nio = NetworkPricePredictor('nio')
+        """Tests _assign data method."""
+        nio = NetworkPricePredictor("nio")
         train, test = nio._split_data()
         for _ in range(4):
-            self.assertIsInstance(
-                _assign_data(train, test)[_],
-                list
-            )
+            self.assertIsInstance(_assign_data(train, test)[_], list)
         self.assertEqual(
-            len(_assign_data(train, test)[0]),
-            len(_assign_data(train, test)[2])
+            len(_assign_data(train, test)[0]), len(_assign_data(train, test)[2])
         )
         self.assertEqual(
-            len(_assign_data(train, test)[1]),
-            len(_assign_data(train, test)[3])
+            len(_assign_data(train, test)[1]), len(_assign_data(train, test)[3])
         )
         self.assertEqual(
-            _assign_data(train, test)[0][1],   # P_t   (training data x), assert P_t+1 = P_t
-            _assign_data(train, test)[2][0]    # P_t+1 (training data y)
+            _assign_data(train, test)[0][
+                1
+            ],  # P_t   (training data x), assert P_t+1 = P_t
+            _assign_data(train, test)[2][0],  # P_t+1 (training data y)
         )
         self.assertEqual(
-            _assign_data(train, test)[1][1],   # P_t   (testing data x), assert P_t+1 = P_t
-            _assign_data(train, test)[3][0]    # P_t+1 (testing data y)
+            _assign_data(train, test)[1][
+                1
+            ],  # P_t   (testing data x), assert P_t+1 = P_t
+            _assign_data(train, test)[3][0],  # P_t+1 (testing data y)
         )
 
     def test_whole_data(self):
-        """Tests whole data method and remaining ones.
-        """
-        nio = NetworkPricePredictor('nio')
+        """Tests whole data method and remaining ones."""
+        nio = NetworkPricePredictor("nio")
         prices = nio._whole_data()
-        self.assertIsInstance(
-            prices,
-            np.ndarray
-        )
+        self.assertIsInstance(prices, np.ndarray)
         self.assertEqual(
             len(_assignment_for_future_prediction(prices)[0]),
-            len(_assignment_for_future_prediction(prices)[1])
+            len(_assignment_for_future_prediction(prices)[1]),
         )
         for n in range(2):
-            self.assertIsInstance(
-                _assignment_for_future_prediction(prices)[n], 
-                list
-            )
+            self.assertIsInstance(_assignment_for_future_prediction(prices)[n], list)
         self.assertEqual(
-            _assignment_for_future_prediction(prices)[0][1], # P_t (training)
-            _assignment_for_future_prediction(prices)[1][0]  # P_t+1 (testing) => assert reccursive form
+            _assignment_for_future_prediction(prices)[0][1],  # P_t (training)
+            _assignment_for_future_prediction(prices)[1][
+                0
+            ],  # P_t+1 (testing) => assert reccursive form
         )
-            
-if __name__ == '__main__':
-    unittest.main()
 
+
+if __name__ == "__main__":
+    unittest.main()
